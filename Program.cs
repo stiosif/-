0784c20280fb04d2a7ed4eb3b1f990abda9c7bd7
stiosif <158315928+stiosif@users.noreply.github.com>
2024-02-01 12:21:51 +0300

    это тут обезьяна какает

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаб2_зад1_САВДЕЙ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите координату X: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите координату Y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            bool result = CheckPoint(x, y);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+
+        static bool CheckPoint(double x, double y)
+        {
+
+            if (y < 0 && y > x - 1 && y > -x - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
